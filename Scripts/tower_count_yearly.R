464c98c66fcb86c80aac8c547b09f719862f83f6
Nathaniel Goh <gohnathaniel2000@gmail.com>
2019-08-01 17:10:09 -0700

    Added count towers by biome (sort of, counting not 100% sure is accurate yet)

--- Scripts/tower_count_yearly.R
+++ Scripts/tower_count_yearly.R
@@ -33,3 +33,44 @@ plot_tower_count <- ggplot(tower_count_yearly) +
   geom_point(aes(x = YEAR, y = n), size = 1, color = "black") +
   ylab("Number of towers") +
   xlab("Year")
+
+#===========================================================================
+
+output_biome <- data.frame()
+
+# Split the tower data table based on biome
+split_tower_list <- split(towers_coords_df, towers_coords_df$BIOME)
+
+# Loop through the list
+for (l in split_tower_list) {
+  for (row in 1:nrow(l)) {
+    
+    # Temp variables
+    tower_site = towers_coords_df[row,]
+    tower_id = tower_site[1, 'SITE_ID']
+    start_year = tower_site[1, 'FLUX_MEASUREMENTS_DATE_START']
+    end_year = tower_site[1, 'FLUX_MEASUREMENTS_DATE_END']
+    biome = tower_site[1, 'BIOME']
+    
+    # Make table of towers at n year for each row
+    tower_table <- expand.grid(c(tower_id), seq(start_year, end_year))
+    tower_table$BIOME <- biome
+    
+    output_biome <- bind_rows(output_biome, tower_table)
+  }
+}
+
+# Rename columns 
+names(output_biome) <- c('SITE_ID', 'YEAR', 'BIOME')
+
+# Get tower count per year
+tower_count_yearly_biome <- output_biome %>%
+  group_by(YEAR, BIOME) %>%
+      summarise(n = n())
+
+# Graph the lineplot for visual representation of tower network over time
+plot_tower_count_biome <- ggplot(tower_count_yearly_biome) +
+  geom_line(aes(x = YEAR, y = n, group = BIOME, color = BIOME)) +
+  geom_point(aes(x = YEAR, y = n), size = 1, color = "black") +
+  ylab("Number of towers") +
+  xlab("Year")
