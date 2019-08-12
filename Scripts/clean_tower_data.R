bc1258e8f8b2c5814db27adda33c9c1b56d6a924
Nathaniel Goh <gohnathaniel2000@gmail.com>
2019-08-12 15:30:37 -0700

    Update clean_tower_data.R

--- Scripts/clean_tower_data.R
+++ Scripts/clean_tower_data.R
@@ -6,10 +6,12 @@ towers <- read.csv('./Data/Towers/Sites_all_BAMS_15march2019_v2.csv', stringsAsF
 # Clean the tower data and pick out the main things we want from the dataset
 towers <- towers %>% 
   filter(IGBP %in% c("WET", "CRO - Rice")) %>%
-    filter(Data_acquired == "yes") %>%
+    # filter(Data_acquired == "yes") %>%
       filter(SITE_ID != "--*") %>%
       filter(SITE_ID != "--") %>%
       filter(SITE_ID != "--**") %>%
+      filter(FLUX_MEASUREMENTS_DATE_START != "--") %>%
+      filter(FLUX_MEASUREMENTS_DATE_END != "--") %>%
         # Count number of data years
         mutate(FLUX_MEASUREMENTS_DATE_END = ifelse(FLUX_MEASUREMENTS_DATE_END=="Present", 2019, FLUX_MEASUREMENTS_DATE_END)) %>%
           mutate(FLUX_MEASUREMENTS_DATE_END = as.numeric(FLUX_MEASUREMENTS_DATE_END), FLUX_MEASUREMENTS_DATE_START= as.numeric(FLUX_MEASUREMENTS_DATE_START))
