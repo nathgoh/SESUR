f0a2e74cb4a73edd76be51db8437a8a9d2ad5fc6
Nathaniel Goh <gohnathaniel2000@gmail.com>
2019-08-14 15:55:39 -0700

    Update run_project.R

--- Scripts/run_project.R
+++ Scripts/run_project.R
@@ -8,19 +8,30 @@ library(proxy)
 library(here)
 library(ggrepel)
 library(MASS)
+library(vegan)
+library(BBmisc)
 here()
 
-# Read the bioclimatic variables and the tower data
+# Read the bioclimatic variables and the tower data and extract the bioclimatic variables
+# from the location of the tower site
 source('./Scripts/read_bioclim.R')
 
 # Draw the cumulative plot, visual representation of K-S Test
+# Comparison of the global biolclimatic variables against those that are obtained where the towers
+# are located
 source('./Scripts/cumulative_plot.R')
 
 # Do representativeness analyses like eucledian distance on bioclimatic variables
+# Use these results to indicate where the flux tower network is a good/bad or the most 
+# representativeness of a certain part of the world given the current network of towers 
 num_bio = 11
 source('./Scripts/representativeness.R')
 
 # Do tower specific analyses
+# Tower to tower analyses specifcally against towers where methane flux data have not been acquired
+# yet, used for indication of optimal towers that should be added to the methane flux network
+# MDS plot indication shows all the towers and which ones we have acquired or not, also gives us a visual
+# of which clusters of towers would be beneficial to the network
 source('./Scripts/tower_representativeness.R')
 
 # Plot our analysis of the representativeness analyses
