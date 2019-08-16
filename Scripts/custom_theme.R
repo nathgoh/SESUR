8ee2f69267170526c57e7df2527fc923996fa486
Nathaniel Goh <gohnathaniel2000@gmail.com>
2019-08-15 17:20:17 -0700

    Added MDS custom theming

--- Scripts/custom_theme.R
+++ Scripts/custom_theme.R
@@ -24,7 +24,9 @@ theme_map <- function(base_size) {
           legend.box = "horizontal",
           legend.text = element_text(size = 8),
           legend.spacing = unit(0, "mm"),
-          legend.key.size = unit(5, "mm"))
+          legend.key.size = unit(5, "mm"),
+          legend.box.background = element_rect(color = "black"),
+          legend.background = element_blank())
 }
 
 #======================================================================================================================================
@@ -42,8 +44,42 @@ theme_hist <- function(base_size) {
           panel.background = element_rect(fill = '#F5F5F5'),
           
           # Axis
-          axis.title = element_text(size = 12, face = 'bold'),
+          axis.title = element_text(size = 10, face = 'bold'),
           
           # Legend
           legend.position = 'none')
+}
+
+#======================================================================================================================================
+
+# Custom MDS ggplot theme
+theme_mds <- function(base_size) {
+  theme_bw(base_size = base_size) +
+    theme(plot.title = element_text(face = 'bold', size = 16, hjust = 0.5),
+          plot.background = element_rect(fill = 'white'),
+          plot.margin = margin(5, 5, 5, 5, "mm"),
+          
+          # Panel
+          panel.grid.minor = element_line(color = '#D0D0D0'),
+          panel.grid.major = element_line(color = '#D0D0D0'),
+          panel.background = element_rect(fill = '#F5F5F5'),
+          
+          # Axis
+          axis.text.x = element_blank(),  
+          axis.text.y = element_blank(), 
+          axis.ticks = element_blank(),  
+          axis.title.x = element_text(size=12), 
+          axis.title.y = element_text(size=12),
+          
+          # Legend
+          legend.title = element_blank(),
+          legend.key = element_blank(),
+          legend.position="bottom",
+          legend.box = "horizontal",
+          legend.text = element_text(size = 8),
+          legend.spacing = unit(0, "mm"),
+          legend.key.size = unit(5, "mm"),
+          legend.box.background = element_rect(color = "black"),
+          legend.background = element_blank())
+
 }
