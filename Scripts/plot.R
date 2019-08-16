8554a30831802971b01ea634c42e3dafd3229283
Nathaniel Goh <gohnathaniel2000@gmail.com>
2019-08-15 17:20:27 -0700

    Added ggplot for MDS

--- Scripts/plot.R
+++ Scripts/plot.R
@@ -62,9 +62,26 @@ ordered_hist <- ggplot() +
 
 #===================================================================================================
 
+# Plot MDS of all towers (acquired and not acquired)
+mds_plot <- ggplot() + 
+  # Add the species labels (bioclimatic variables)
+  geom_text(data = species.scores, aes(x = NMDS1, y = NMDS2, label = species), alpha = 0.5, color = "black") +
+  #geom_point(data = species.scores, aes(x = NMDS1, y = NMDS2, color = "black"), alpha = 0.5) +
+  
+  # Add the point markers (tower sites)
+  geom_point(data = data.scores, aes(x = NMDS1, y = NMDS2, shape = as.factor(grp), color = as.factor(grp)), size = 3) + 
+  
+  # Add the site labels (tower ID's)
+  geom_text_repel(data = data.scores, aes(x = NMDS1, y = NMDS2, label = towers_coords_df_all$SITE_ID, color = as.factor(grp)), size = 3, vjust = 0, hjust = 0) + 
+  scale_colour_manual(values = c("No" = "red", "Yes" = "blue")) +
+  coord_equal() +
+  theme_mds(12)
+
+#===================================================================================================
+
 # mds_plot <- ggplot() +
 #   geom_point(data = towers_mds, aes(x = points, fill = points), size = 1) +
 #   scale_color_manual(values = c(towers_mds$))
-
-plot(towers_mds$points, type = "n", pch = 20, cex = 3, col = adjustcolor("black", alpha = 0.3), xlab = "X", ylab = "Y") 
-text(towers_mds$points, labels = towers_coords_df_mds$SITE_ID, cex = 0.75)
+# 
+# plot(towers_mds$points, type = "n", pch = 20, cex = 3, col = adjustcolor("black", alpha = 0.3), xlab = "X", ylab = "Y") 
+# text(towers_mds$points, labels = towers_coords_df_mds$SITE_ID, cex = 0.75)
