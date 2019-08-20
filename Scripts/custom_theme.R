caad4d77557153395b88a48519383d8f2e7c2945
Nathaniel Goh <gohnathaniel2000@gmail.com>
2019-08-19 17:10:31 -0700

    Added theme for single climatic ggplot

--- Scripts/custom_theme.R
+++ Scripts/custom_theme.R
@@ -20,7 +20,7 @@ theme_map <- function(base_size) {
           # Legend
           legend.title = element_blank(),
           legend.key = element_blank(),
-          legend.position="bottom",
+          legend.position= c(0.1, 0.2),
           legend.box = "horizontal",
           legend.text = element_text(size = 8),
           legend.spacing = unit(0, "mm"),
@@ -74,12 +74,43 @@ theme_mds <- function(base_size) {
           # Legend
           legend.title = element_blank(),
           legend.key = element_blank(),
-          legend.position="bottom",
-          legend.box = "horizontal",
+          legend.position= c(0.1, 0.2),
+          legend.box = "vertical",
           legend.text = element_text(size = 8),
           legend.spacing = unit(0, "mm"),
           legend.key.size = unit(5, "mm"),
           legend.box.background = element_rect(color = "black"),
           legend.background = element_blank())
 
+}
+
+#===================================================================================================
+
+# Custom single climatic ggplot theme
+theme_single <- function(base_size) {
+  theme_bw(base_size = base_size) +
+    theme(plot.title = element_text(face = 'bold', size = 16, hjust = 0.5),
+          plot.background = element_rect(fill = 'white'),
+          plot.margin = margin(5, 5, 5, 5, "mm"),
+          
+          # Panel
+          panel.grid.minor = element_line(color = '#D0D0D0'),
+          panel.grid.major = element_line(color = '#D0D0D0'),
+          panel.background = element_rect(fill = '#F5F5F5'),
+          
+          # Axis
+          axis.text = element_blank(),
+          axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          
+          # Legend
+          legend.title = element_blank(),
+          legend.key = element_blank(),
+          legend.position= c(0.1, 0.2),
+          legend.box = "vertical",
+          legend.text = element_text(size = 8),
+          legend.spacing = unit(0, "mm"),
+          legend.key.size = unit(5, "mm"),
+          legend.box.background = element_rect(color = "black"),
+          legend.background = element_blank())
 }
